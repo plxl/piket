b0d68f19b1443451af6f88ed2858f3ce8ad28c12
plxl <210124508+plxl@users.noreply.github.com>
2025-10-21 22:59:36 +0800

    UPDATE MarchingPikmin module __init__ to import TreasureSprite class

--- src/piket/marching_pikmin/__init__.py
+++ src/piket/marching_pikmin/__init__.py
@@ -1,5 +1,6 @@
 from .level import Level
 from .tile import Tile
 from .piki import Piki
+from .treasure import TreasureSprite
 
-__all__ = ["Level", "Tile", "Piki"]
+__all__ = ["Level", "Tile", "Piki", "TreasureSprite"]
